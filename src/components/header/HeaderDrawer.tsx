9d3f3b402e90844314501d60917587229354fec1
James Wienecke <james.a.wienecke@gmail.com>
2022-03-01 16:16:55 -0600

    Broke the drawer out into its own component.

---
+++ src/components/header/HeaderDrawer.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {Link as RouterLink} from "react-router-dom";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Button,
+  IconButton,
+  VStack,
+  Text,
+  DrawerFooter,
+} from "@chakra-ui/react";
+import {MdMenu} from "react-icons/md";
+import ColorModeToggle from "./navmenu/ColorModeToggle";
+
+// const p = 15;
+// placement = "right",
+//   width,
+//   isOpen,
+//   children,
+//   onClose,
+//   btnRef,
+//   title = "Menu",
+//   footer,
+
+interface props {
+  btnRef: React.RefObject<HTMLButtonElement>;
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  footer: React.ReactNode;
+}
+
+const HeaderDrawer = ({btnRef, isOpen, onClose, children, footer}: props) => {
+  return (
+    <Drawer
+      isOpen={isOpen}
+      onClose={onClose}
+      finalFocusRef={btnRef}
+      placement={"right"}
+    >
+      <DrawerOverlay/>
+      <DrawerContent alignItems="center">
+        <DrawerCloseButton alignSelf="end" mx={"15px"} my={"15px"}/>
+        <DrawerHeader my={"15px"}>
+          <Text as="p">James Wienecke</Text>
+        </DrawerHeader>
+        <DrawerBody>
+          {children}
+        </DrawerBody>
+        <DrawerFooter>
+          {footer}
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default HeaderDrawer;
--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -1,53 +1,34 @@
 import React from "react";
 import {Link as RouterLink} from "react-router-dom";
 import {
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
   Flex,
   Link,
   Button,
-  IconButton, useDisclosure, VStack, Text,
+  IconButton,
+  useDisclosure,
+  VStack,
+  Text,
 } from "@chakra-ui/react";
 import {MdMenu} from "react-icons/md";
 import ColorModeToggle from "./navmenu/ColorModeToggle";
+import HeaderDrawer from "./HeaderDrawer";
 
 
 const MobileHeader = () => {
   const {isOpen, onOpen, onClose} = useDisclosure();
-  const btnRef = React.useRef(null);
+  const btnRef = React.useRef<HTMLButtonElement>(null);
 
   return (
     <Flex as={"nav"} display={{base: "flex", md: "none"}} justify={"flex-end"}>
       <Button ref={btnRef} onClick={onOpen} as={IconButton} icon={<MdMenu/>}/>
-      <Drawer
-        isOpen={isOpen}
-        onClose={onClose}
-        finalFocusRef={btnRef}
-        placement={"right"}
-      >
-        <DrawerOverlay/>
-        <DrawerContent alignItems="center">
-          <DrawerCloseButton alignSelf="end" mx={"15px"} my={"15px"}/>
-          <DrawerHeader my={"15px"}>
-            <Text as="p">James Wienecke</Text>
-          </DrawerHeader>
-          <DrawerBody>
-            <VStack alignItems="left">
-              <Button variant='text'>Home</Button>
-              <Button variant='text'>About</Button>
-              <Button variant='text'>Work</Button>
-              <Button variant='text'>Contact me!</Button>
-              <ColorModeToggle/>
-            </VStack>
-          </DrawerBody>
-          {/*<DrawerFooter></DrawerFooter>*/}
-        </DrawerContent>
-      </Drawer>
+      <HeaderDrawer btnRef={btnRef} isOpen={isOpen} onClose={onClose} footer={<ColorModeToggle/>}>
+        <VStack alignItems="left">
+          <Button variant='text'>Home</Button>
+          <Button variant='text'>About</Button>
+          <Button variant='text'>Work</Button>
+          <Button variant='text'>Contact me!</Button>
+        </VStack>
+      </HeaderDrawer>
     </Flex>
   );
 }
