28c79ee46c10ca2e66f316d055a64a7074b92027
James Wienecke <james.a.wienecke@gmail.com>
2022-03-03 14:26:53 -0600

    Continuing to build out content for body. Welcome section is coming along.

--- src/components/Body/Article/Article.tsx
+++ src/components/Body/Article/Article.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import {Box, Container, Flex, Heading} from "@chakra-ui/react";
 
 interface props {
   children: React.ReactNode;
   id?: string;
+  title: string;
 }
 
-const Article = ({children, id}: props) => {
+const Article = ({children, id, title}: props) => {
   return (
-    <section id={id}>
-      {children}
-    </section>
+    <Box
+      maxW={{base: "95vw", md: "75vw"}}
+      p={{base: 2, md: 8}}
+      id={id}
+      alignContent={"center"}
+    >
+      <Heading
+        textAlign={{base: "center", md: "left"}}
+        mb={4}
+      >
+        {title}
+      </Heading>
+      <Flex wrap={{base: "wrap", md: "nowrap"}}>
+        {children}
+      </Flex>
+    </Box>
   )
 }
 
--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import {Container} from "@chakra-ui/react";
+import {Box, Container, Divider, Icon, Image, Link, Text} from "@chakra-ui/react";
 import Article from "./Article";
+import { ImGithub } from "react-icons/im";
 
-function Body() {
+
+const Body = () => {
 return (
-  <Container as={"main"}>
-    <Article id={"top"}>
-      brief home article
+  <Box as={"main"}>
+    <Article id={"top"} title={"Welcome"}>
+      <Text
+        mx={{base: "auto", md: "inherit"}}
+        mr={{base: "inherit", md: 8}}
+        mb={4}
+      >
+        My name is James Wienecke! I am a software developer from San Antonio, Texas.
+        I do front and back end web development and design, which I am passionate about.
+        I'm always learning new technology, so check out {' '}
+        <Link href="https://github.com/james-wienecke" isExternal>
+          my github <Icon as={ImGithub} mr={2}/>
+        </Link>
+        to see what I've been getting into lately.
+        <br/>
+        <br/>
+        If there's anything I can do to help you, please feel free to reach out —
+        and thank you for checking out my page!
+      </Text>
+      <Image
+        src={"https://via.placeholder.com/150.png"}
+        alt={"placeholder picture for James's headshot"}
+        borderRadius={"150px"}
+        boxSize={"150px"}
+        mx={{base: "auto", md: "inherit"}}
+        ml={{base: "auto", md: 8}}
+      />
     </Article>
-    <Article id={"about"}>
+    <Article id={"about"} title={"About me"}>
       about me
     </Article>
-    <Article id={"work"}>
+    <Article id={"work"} title={"My work"}>
       projects and skills
     </Article>
-    <Article id={"contact"}>
+    <Article id={"contact"} title={"Get in touch!"}>
       contact info
     </Article>
-  </Container>
+  </Box>
 );
 }
 
